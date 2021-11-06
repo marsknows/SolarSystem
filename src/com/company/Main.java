03d55749fce87e4770961e05d025961d4fc0e139
marsknows <88752025+marsknows@users.noreply.github.com>
2021-11-06 09:56:40 -0500

    Solar System Basic Structure is Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem andromeda = new SolarSystem();
+        andromeda.Sun=1;
+        andromeda.planet=2;
+        andromeda.moon=4;
+    }
+}
